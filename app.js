51ced987e96d476bc45a33b416fb06dceeb8bf82
Morgan Miller <morganm.ewc@gmail.com>
2020-08-18 19:00:33 -0400

    more progress

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log ("this works") 
+ let timeLeft = 60;
+ let timer   
+let currentQuestionIndex = 0;
+
+let questions = [
+    {
+        question: "What color is the sky?", 
+        choices: ["blue","red", "purple", "orange"], 
+        answer: "blue"
+    },
+    {
+        question: "What is the color of grass?",
+        choices: ["green", "pink", "blue", "orange"],
+        answer: "green"
+
+    }
+]
+
+console.log(questions [currentQuestionIndex].question) 
+console.log(questions [currentQuestionIndex].choices)
+console.log(questions [currentQuestionIndex].answer)
+
+
+$("#start-button").on("click", startQuiz)
+function startQuiz() {
+    $("#start-button").hide()
+    timer = setInterval (timeDrop, 1000)
+}
+function timeDrop() {
+timeLeft -- 
+console.log (timeLeft)
+}
